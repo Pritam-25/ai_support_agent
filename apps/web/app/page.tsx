03c65510be19ff286bced05b3aa511e12ba32e18
Silly_coder <maityp394@gmail.com>
2025-08-15 23:14:32 +0530

    feat: add convex database

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,48 +1,39 @@
-"use client"
+"use client";
 import { Button } from "@workspace/ui/components/button"
-import { Input } from "@workspace/ui/components/input"
-import { add } from "@workspace/math/add"
+import {useMutation, useQuery} from "convex/react"
+import {api} from "@workspace/backend/convex/_generated/api"
 import { useState } from "react";
+import { Input } from "@workspace/ui/components/input";
 
 export default function Page() {
-  const [firstNumber, setFirstNumber] = useState<string>('');
-  const [secondNumber, setSecondNumber] = useState<string>('');
-  const [result, setResult] = useState<number | string>('');
 
-  const handleAdd = () => {
-    const num1 = parseFloat(firstNumber);
-    const num2 = parseFloat(secondNumber);
-    if (!isNaN(num1) && !isNaN(num2)) {
-      setResult(add(num1, num2));
-    } else {
-      setResult('Invalid input');
-    }
+   const [name, setName] = useState(""); // Track user input
+
+  const users = useQuery(api.user.getManyUsers);
+  const addUser = useMutation(api.user.addUser);
+
+  const handleAddUser = async () => {
+    if (!name.trim()) return; // Prevent empty names
+    await addUser({ name });
+    setName(""); // Clear input after adding
   };
 
   return (
     <div className="flex items-center justify-center min-h-svh">
       <div className="flex flex-col items-center justify-center gap-4">
-        <h1 className="text-2xl font-bold">Hello World</h1>
-
-        <Input
-          placeholder="First number"
-          className="w-64"
-          value={firstNumber}
-          onChange={(e) => setFirstNumber(e.target.value)}
+        <h1 className="text-2xl font-bold">Apps/Web</h1>
+         <Input
+          type="text"
+          placeholder="Enter your name"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
         />
 
-        <Input
-          placeholder="Second number"
-          className="w-64"
-          value={secondNumber}
-          onChange={(e) => setSecondNumber(e.target.value)}
-        />
-
-        <Button size="sm" onClick={handleAdd}>
-          Add
+        <Button size="lg" variant="default" onClick={handleAddUser}>
+          Add User
         </Button>
 
-        <p className="text-lg font-semibold">{result}</p>
+        <pre>{JSON.stringify(users, null, 2)}</pre>
       </div>
     </div>
   );
